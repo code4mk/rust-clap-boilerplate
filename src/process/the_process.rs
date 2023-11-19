f754a163973332484a8299a70d08226435f2c7f4
Mostafa Kamal <hiremostafa@gmail.com>
2023-11-19 22:11:12 +0600

    fix path

--- src/process/the_process.rs
+++ src/process/the_process.rs
@@ -1,5 +1,5 @@
 use clap::ArgMatches;
-use crate::commands::register_person::process_command as register_user;
+use crate::my_commands::register_person::process_command as register_user;
 
 pub fn process_commands(match_result: &ArgMatches) {
   register_user(match_result)
